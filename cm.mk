d7e73c068b7ad399b603eea3301a9f2d079dd7ed
Azimoto9 <azi9@cock.li>
2016-01-16 02:13:53 -0800

    mkvendor.sh: Initial commit of cprimeltemtr

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := cprimeltemtr
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/cprimeltemtr/device_cprimeltemtr.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := cprimeltemtr
+PRODUCT_NAME := cm_cprimeltemtr
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := cprimeltemtr
+PRODUCT_MANUFACTURER := samsung
